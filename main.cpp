401f48d2b20367e5b5f3b1ddfbbd2e06bb7f833b
Thorsten Mindrup <mini_coe@freenet.de>
2022-05-04 23:04:28 +0200

    add my_class for main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
+#include "myHeader.h"
 #include <iostream>
+
+using namespace New;
+using namespace std;
+
+void my_class::present_figure()
+{
+	cout << "Present figure" << endl;
+}
 
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	my_class mc;
+	mc.present_figure();
+	//cout << "Hello World!" << endl;
 	return 0;
 }
---
+++ myHeader.h
@@ -0,0 +1,9 @@
+namespace New
+{
+	class my_class
+	{
+		public:
+			void present_figure();
+	}	
+
+}
